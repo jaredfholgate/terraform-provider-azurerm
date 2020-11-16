82d18128ffa71f43789ceaa8f8edd51db751e44a
tombuildsstuff <git@ibuildstuff.co.uk>
2020-11-16 12:18:14 +0100

    sdk: consolidating the service registration interface
    Introducing a Typed version, in addition to the Untyped version

--- azurerm/internal/provider/services.go
+++ azurerm/internal/provider/services.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/sdk"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/advisor"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/analysisservices"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/apimanagement"
@@ -15,7 +16,6 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/bot"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/cdn"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/cognitive"
-	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/common"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/compute"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/containers"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/cosmos"
@@ -88,8 +88,8 @@ import (
 
 //go:generate go run ../tools/generator-services/main.go -path=../../../
 
-func SupportedServices() []common.ServiceRegistration {
-	return []common.ServiceRegistration{
+func SupportedServices() []sdk.UntypedServiceRegistration {
+	return []sdk.UntypedServiceRegistration{
 		advisor.Registration{},
 		analysisservices.Registration{},
 		apimanagement.Registration{},
